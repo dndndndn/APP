e85f2eda9ba8a628401f39531d061671421018f8
fanwnehu <16031138@buaa.edu.cn>
2019-03-24 15:09:13 +0800

    my application

---
+++ MyApplication3/MyApplication3/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.myapplication;
+
+
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.myapplication.R;
+/*任务在主界面的Hello World位置显示：用户名+“登录成功”*/
+public class MainActivity extends AppCompatActivity {
+    private TextView et_user_name;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        et_user_name = findViewById(R.id.et_user_name);
+        Intent intent=new Intent(MainActivity.this,LoginActivity.class);
+        startActivityForResult(intent,1);
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (data!=null){
+            String userName = data.getStringExtra("userName");
+            //if (!TextUtils.isEmpty(userName)){
+            Toast.makeText(MainActivity.this,"登陆成功："+userName, Toast.LENGTH_SHORT).show();
+            et_user_name.setText(userName);
+            //}
+        }
+    }
+}
